b5a1847d03e4b84684cc44ec09479a60b8de32e2
moihack <moihack.inside@gmail.com>
2016-05-29 17:12:05 +0300

    initial name change
    Change-Id: I230e11d67b8ff0c54e2af910be3044649e1a75b4

--- k1_turbo-vendor.mk
+++
@@ -1,9 +0,0 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/bin,system/bin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/etc,system/etc)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/lib,system/lib)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/xbin,system/xbin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/vendor,system/vendor)
