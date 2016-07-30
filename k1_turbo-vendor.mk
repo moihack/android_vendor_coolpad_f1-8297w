a98e22ed131acdb20d4030346798c803b4e5f40d
fire855 <thefire855@gmail.com>
2016-07-30 17:53:14 +0200

    Add NFC app & blobs

--- k1_turbo-vendor.mk
+++ k1_turbo-vendor.mk
@@ -1,3 +1,5 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/app,system/app)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/bin,system/bin)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/kingzone/k1_turbo/proprietary/etc,system/etc)
