4dd8735089ca1f2d18e6056272f2a6bdcd2113c4
moihack <moihack.inside@gmail.com>
2016-08-11 15:42:35 +0300

    initial name changes & removed nfc

---
+++ f1_8297w-vendor.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/f1_8297w/proprietary/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/f1_8297w/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/f1_8297w/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/f1_8297w/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/f1_8297w/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/coolpad/f1_8297w/proprietary/vendor,system/vendor)
